7f54ec5e9d320ba5980e235c565e3a2834e29ee7
anjaliParashar <41367634+anjaliParashar@users.noreply.github.com>
2023-07-23 15:23:45 -0400

    Add files via upload

---
+++ script_copy.py
@@ -0,0 +1,99 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage.morphology import distance_transform_edt as bwdist
+import pickle
+
+goal = [400, 50]
+start = [50, 350]
+nrows = 512
+ncols = 1024
+[x, y] = np.meshgrid (np.arange(ncols), np.arange(nrows))
+# Generate some points
+def obstacle_gen(rec1, rec2, cir1,cir2):
+    obstacle = np.zeros((nrows, ncols))
+    # Generate some obstacle
+    
+    obstacle [rec1[0]:rec1[0]+100, rec1[1]:rec1[1]+200] = True
+    obstacle [rec2[0]:rec2[0]+50, rec2[1]:rec2[1]+100] = True
+
+    t = ((x - cir1[0])**2 + (y - cir1[1])**2) < cir1[2]**2
+    obstacle[t] = True
+
+    t = ((x - cir2[0])**2 + (y - cir2[1])**2) < cir2[2]**2
+    obstacle[t] = True
+    return obstacle
+
+def potential(obstacle):
+    d = bwdist(obstacle==0)
+    # Rescale and transform distances
+    d2 = (d/100.) + 1
+    d0 = 2
+    nu = 800
+    repulsive = nu*((1./d2 - 1/d0)**2)
+    repulsive [d2 > d0] = 0
+    xi = 1/700.
+    attractive = xi * ( (x - goal[0])**2 + (y - goal[1])**2 )
+    f = attractive + repulsive
+    return f
+
+def GradientBasedPlanner (f, start_coords, end_coords, max_its):
+    [gy, gx] = np.gradient(-f)
+
+    route = np.vstack( [np.array(start_coords), np.array(start_coords)] )
+    for i in range(max_its):
+        current_point = route[-1,:]
+#         print(sum( abs(current_point-end_coords) ))
+        if sum( abs(current_point-end_coords) ) < 5.0:
+            print('Reached the goal !')
+            break
+        ix = int(round( current_point[1] ))
+        iy = int(round( current_point[0] ))
+        vx = gx[ix, iy]
+        vy = gy[ix, iy]
+        dt = 1 / np.linalg.norm([vx, vy])
+        next_point = current_point + dt*np.array( [vx, vy] )
+        route = np.vstack( [route, next_point] )
+    route = route[1:,:] 
+    return route
+
+def coordinate_gen():
+    rect_10 = np.random.randint(low=200, high = 300)
+    rect_11 = np.random.randint(low=100, high = 300)
+    rect_20 = np.random.randint(low=100, high = 200)
+    rect_21 = np.random.randint(low=300, high = 400)
+    rec1 = np.array([rect_10, rect_11])
+    rec2 = np.array([rect_20, rect_21])
+    cir13 = np.random.randint(low=50, high = 60)
+    cir23 = np.random.randint(low=60, high = 80)
+    cir10 = np.random.randint(low=100, high = 300)
+    cir11 = np.random.randint(low=20, high = 150)
+    cir20 = np.random.randint(low=10, high = 200)
+    cir21 = np.random.randint(low=10, high = 200)
+    cir1 = np.array([cir10,cir11,cir13])
+    cir2 = np.array([cir20,cir21,cir23])
+    return rec1,rec2, cir1, cir2
+
+route_list = []
+obstacle_list = []
+
+N_data = 10000
+for i in range(N_data):
+    rec1,rec2, cir1, cir2 = coordinate_gen()
+    obstacle = obstacle_gen(rec1,rec2, cir1, cir2)
+    f = potential(obstacle=obstacle)
+    route = GradientBasedPlanner(f, start, goal, 700)
+    length = route.shape[0]
+    padding = np.tile(route[-1,:], (701-length, 1)) 
+    route = np.vstack( [route, padding] )
+    if sum(abs(route[-1,:]-goal))<10.0:
+        route_list.append(route)
+        obstacle_list.append(obstacle)
+    if i%3000==0:
+        print('Check')
+
+data = {"route":route_list, "obstacle":obstacle_list}
+print('length of data:', len(route_list))
+f_data = "train_data/file_1.pkl"
+with open(f_data, 'wb') as fp:
+  pickle.dump(data, fp)
+fp.close()
---
+++ training.py
@@ -0,0 +1,227 @@
+#@markdown ### **Training**
+#@markdown
+#@markdown Takes about an hour. If you don't want to wait, skip to the next cell
+#@markdown to load pre-trained weights
+import torch
+from network_action import ConditionalUnet1D
+import numpy as np
+import torch.nn as nn
+from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
+from diffusers.optimization import get_scheduler
+from tqdm.auto import tqdm
+from dataset_gen import TrajectoryDataset
+#from action_latent import Encoder, Decoder
+from torch.utils.tensorboard import SummaryWriter
+
+# default `log_dir` is "runs" - we'll be more specific here
+
+#actual data load
+dataset = TrajectoryDataset()
+# create dataloader
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=2,
+    num_workers=1,
+    shuffle=True,
+    # accelerate cpu-gpu transfer
+    pin_memory=True,
+    # don't kill worker process afte each epoch
+    persistent_workers=True
+)
+# visualize data in batch
+batch = next(iter(dataloader))
+print("batch['route'].shape:", batch['route'].shape)
+print("batch['obstacle'].shape", batch['obstacle'].shape)
+print('dataset length:',len(dataloader))
+
+route = batch['route'].float()
+obstacle = batch['obstacle'].float()
+device = torch.device('cuda')
+# Exponential Moving Average
+# accelerates training and improves stability
+# holds a copy of the model weights
+
+obs_horizon = 580
+pred_horizon = 512
+obs_dim = 2
+action_dim = 1024
+# Standard ADAM optimizer
+# Note that EMA parametesr are not optimized
+"""
+noise_pred_net = UNET(
+    in_channels = 1,
+    first_out_channels = 128,
+    exit_channels = 1,
+    downhill = 4,
+    padding=0,
+    global_cond_dim=obs_dim*obs_horizon
+)
+"""
+noise_pred_net = ConditionalUnet1D(
+    input_dim=action_dim,
+    global_cond_dim=obs_dim*obs_horizon
+)
+
+noise_pred_net = noise_pred_net.cuda()
+optimizer = torch.optim.AdamW(
+    params=noise_pred_net.parameters(),
+    lr=1e-4, weight_decay=1e-6)
+# Cosine LR schedule with linear warmup
+num_epochs = 100
+writer = SummaryWriter('runs_/diffusion_'+str(num_epochs))
+
+lr_scheduler = get_scheduler(
+    name='cosine',
+    optimizer=optimizer,
+    num_warmup_steps=500,
+    num_training_steps=len(dataloader) * num_epochs
+)
+
+num_diffusion_iters = 1000
+noise_scheduler = DDPMScheduler(
+    num_train_timesteps=num_diffusion_iters,
+    # the choise of beta schedule has big impact on performance
+    # we found squared cosine works the best
+    beta_schedule='squaredcos_cap_v2',
+    # clip output to [-1,1] to improve stability
+    clip_sample=True,
+    # our network predicts noise (instead of denoised action)
+    prediction_type='epsilon'
+)
+
+
+with tqdm(range(num_epochs), desc='Epoch') as tglobal:
+    # epoch loop
+    for epoch_idx in tglobal:
+        epoch_loss = list()
+        # batch loop
+        with tqdm(dataloader, desc='Batch', leave=False) as tepoch:
+            for nbatch in tepoch:
+                nroute = nbatch['route'].to(device)
+                nobstacle = nbatch['obstacle'].to(device)
+                nroute = nroute.float()
+                nobstacle = nobstacle.float()
+                B = nroute.shape[0]
+                
+                route_cond = nroute[:,:obs_horizon,:]
+                # (B, obs_horizon * obs_dim)
+                route_cond = route_cond.flatten(start_dim=1)
+
+                # sample noise to add to actions
+                noise = torch.randn(nobstacle.shape, device=device)
+
+                # sample a diffusion iteration for each data point
+                timesteps = torch.randint(
+                    0, noise_scheduler.config.num_train_timesteps,
+                    (B,), device=device
+                ).long()
+
+                #print(timesteps.shape)
+                noisy_obstacle = noise_scheduler.add_noise(
+                nobstacle, noise, timesteps)
+                #print(noisy_actions.shape,obs_cond.shape)
+            # predict the noise residual
+                noisy_obstacle.to(device)
+                #print(noisy_actions.shape)
+                timesteps.to(device)
+                route_cond.to(device)
+                noise_pred = noise_pred_net(
+                                sample=noisy_obstacle,
+                                timestep=timesteps,
+                                global_cond= route_cond #obs.flatten(start_dim=1)
+                            ).to(device)
+            #noise_pred = noise_pred_net(
+            #    noisy_actions, timesteps, global_cond=obs_cond).to(device)
+            #noise_pred = noise_pred
+            
+            # L2 loss
+                loss = nn.functional.mse_loss(noise_pred, noise)
+
+            # optimize
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+                # step lr scheduler every batch
+                # this is different from standard pytorch behavior
+                lr_scheduler.step()
+
+                # update Exponential Moving Average of the model weights
+                #ema.step(noise_pred_net)
+
+                # logging
+                loss_cpu = loss.item()
+                epoch_loss.append(loss_cpu)
+                tepoch.set_postfix(loss=loss_cpu)
+        tglobal.set_postfix(loss=np.mean(epoch_loss))
+        writer.add_scalar('training loss',
+                np.mean(epoch_loss),
+                epoch_idx)
+"""
+                # observation as FiLM conditioning
+                # (B, obs_horizon, obs_dim)
+                obs_cond = nroute[:,:obs_horizon,:]
+                # (B, obs_horizon * obs_dim)
+                obs_cond = obs_cond.flatten(start_dim=1)
+
+                # sample noise to add to actions
+                noise = torch.randn(nroutetacle.shape, device=device)
+            
+                # sample a diffusion iteration for each data point
+                timesteps = torch.randint(
+                    0, noise_scheduler.config.num_train_timesteps,
+                    (B,), device=device
+                ).long()
+                #print('timesteps',timesteps.shape)
+                # add noise to the clean images according to the noise magnitude at each diffusion iteration
+                # (this is the forward diffusion process)
+                noisy_actions = noise_scheduler.add_noise(
+                    nroutetacle, noise, timesteps)
+                # predict the noise residual
+                noisy_actions.to(device)
+                timesteps.to(device)
+                obs_cond.to(device)
+                noise_pred = noise_pred_net(
+                                sample=noisy_actions,
+                                timestep=timesteps,
+                                global_cond=obs.flatten(start_dim=1)
+                            ).to(device)
+                #noise_pred = noise_pred_net(
+                #    noisy_actions, timesteps, global_cond=obs_cond).to(device)
+                #noise_pred = noise_pred
+                
+                # L2 loss
+                loss = nn.functional.mse_loss(noise_pred, noise)
+
+                # optimize
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+                # step lr scheduler every batch
+                # this is different from standard pytorch behavior
+                lr_scheduler.step()
+
+                # update Exponential Moving Average of the model weights
+                #ema.step(noise_pred_net)
+
+                # logging
+                loss_cpu = loss.item()
+                epoch_loss.append(loss_cpu)
+                tepoch.set_postfix(loss=loss_cpu)
+        tglobal.set_postfix(loss=np.mean(epoch_loss))
+        writer.add_scalar('training loss',
+                    sum(epoch_loss) / len(dataloader),
+                    epoch_idx)
+"""
+# Additional information
+EPOCH = num_epochs
+PATH = "static_obstacle_model_"+str(num_epochs)+".pt"
+LOSS = np.mean(epoch_loss)
+
+torch.save({
+            'epoch': EPOCH,
+            'model_state_dict': noise_pred_net.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': LOSS,
+            }, PATH)
+        
+
